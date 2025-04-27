6006b8b91ced9d8ec3e899903a8b552d56a933b5
Yeicob99 <yosoyjacobo23@gmail.com>
2025-04-26 23:47:21 -0400

    Added the project

---
+++ to-do-list/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+
+//Creamos la funcion App, que es la funcion principal de la aplicacion
+function App() {
+
+//Creamos estado para gurdar la tarea
+const [task, setTask] = useState('');
+const [tasks, setTasks] = useState([]); //pregunta
+
+
+//funcion para manejar el evento de agregar una tarea
+const handleAddTask = () => {
+  if (task.trim() === '') return;
+
+ // Funcion para filtrar las tareas para eliminar la tarea con el id correspondiente
+const taskDelete = (id) => {
+  const newTasks = tasks.filter(task=> task.id !== id);
+  setTasks(newTasks);
+  console.log(newTasks);
+}
+
+  const newTask = {
+    id: Date.now(),
+    text: task,
+  };
+  //Agregamos la nueva tarea al estado de tareas
+  setTasks([...tasks, newTask]);
+  console.log(newTask);
+  setTask(''); 
+
+}
+
+  return (
+    <div className="app-container">
+      <h1>Mi To-Do List</h1>
+      <input 
+        type="text" 
+        placeholder="Escribe una tarea"
+        value={task}
+        onChange = {(e) => setTask(e.target.value)}
+      />
+      <button onClick={handleAddTask}>add task</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}
+            <button> Eliminar </button>
+            </li>
+      ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
